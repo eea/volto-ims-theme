88bb3abbfa5c386087d8edf4d511606052a046da
David Ichim <ichim.david@gmail.com>
2021-07-07 18:47:06 +0300

    Refs #135559 changed the following:
    - Unauthorized.jsx:
      - push history change after 10 seconds letting the user go directly or hitting the
      Go back button to go back to the previous url
      - use an effect with manual history change in order to still get the proper 401 error
      for the unauthorized page
    - utils.overrides:
      - added this file in extras folder as an override for the utils.less of pastanaga theme
    - custom.overrides:
      - import utils.overrides since pastanaga doesn't load ui overrides

--- src/customizations/components/theme/Unauthorized/Unauthorized.jsx
+++ src/customizations/components/theme/Unauthorized/Unauthorized.jsx
@@ -2,24 +2,74 @@
  * @module components/theme/Unauthorized/Unauthorized
  */
 
-import React from 'react';
-import { useLocation, Redirect } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useLocation, Link, useHistory } from 'react-router-dom';
 import { getBaseUrl } from '@plone/volto/helpers';
+import { Container, Button } from 'semantic-ui-react';
+
+import { FormattedMessage } from 'react-intl';
+
+import { withServerErrorCode } from '@plone/volto/helpers/Utils/Utils';
 
 /**
  * unauthorized function.
  * @function Unauthorized
  * @returns {string} Markup of the unauthorized page.
  */
+
 const Unauthorized = () => {
-  let location = useLocation();
+  const location = useLocation();
+  const pathname = location.pathname;
+  const base_pathname = getBaseUrl(pathname);
+  const login = `${base_pathname}/login`;
+  const history = useHistory();
+  useEffect(() => {
+    const login_with_return = login + `?return_url=${pathname}`;
+    setTimeout(() => {
+      return history.push(login_with_return);
+    }, 10000);
+  }, [history, login, pathname]);
   return (
-    <Redirect
-      to={`${getBaseUrl(location.pathname)}/login?return_url=${
-        location.pathname
-      }`}
-    />
+    <Container className="view-wrapper">
+      <h1>
+        <FormattedMessage id="Unauthorized" defaultMessage="Unauthorized" />
+      </h1>
+      <h3>
+        <FormattedMessage
+          id="Forbidden, you will be redirected to the login screen in 10 seconds"
+          defaultMessage="Forbidden, you will be redirected to the login screen in 10 seconds"
+        />
+        .
+      </h3>
+
+      <p className="description">
+        <FormattedMessage id="Click" defaultMessage="Click" />
+        <Link
+          className={'ui mini compact button my-half unauthorized-buttons'}
+          to={login}
+        >
+          <FormattedMessage id="Log in" defaultMessage="Log in" />
+        </Link>
+        <FormattedMessage
+          id="to get to login screen or click"
+          defaultMessage="to get to login screen or click"
+        />
+        <Button
+          size={'mini'}
+          compact
+          onClick={() => history.goBack()}
+          className={'my-half unauthorized-buttons'}
+        >
+          <FormattedMessage id="Get back" defaultMessage="Get back" />
+        </Button>
+        <FormattedMessage
+          id="to get to the previous location"
+          defaultMessage="to get to the previous location"
+        />
+        .
+      </p>
+    </Container>
   );
 };
 
-export default Unauthorized;
+export default withServerErrorCode(401)(Unauthorized);
