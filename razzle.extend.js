e32e0f83973f3249dc6e77d3af6e4a7a8b404f44
Alin Voinea <alin.voinea@eaudeweb.ro>
2021-11-17 21:16:01 +0100

    14054 allow the usage of other themes when we also have volto-eea-des… (#49)
    * 14054 allow the usage of other themes when we also have volto-eea-design-system (#47)

--- razzle.extend.js
+++ razzle.extend.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const plugins = (defaultPlugins) => {
   return defaultPlugins;
 };
@@ -5,6 +6,13 @@ const modify = (config, { target, dev }, webpack) => {
   const themeConfigPath = `${__dirname}/theme/theme.config`;
   config.resolve.alias['../../theme.config$'] = themeConfigPath;
   config.resolve.alias['../../theme.config'] = themeConfigPath;
+  const projectRootPath = path.resolve('.');
+  const themeLessPath = `${projectRootPath}/node_modules/@eeacms/volto-eea-design-system/theme`;
+  const semanticLessPath = `${projectRootPath}/node_modules/semantic-ui-less`;
+  const hasDesignSystemInstalled = config.resolve.alias['eea-volto-themes'];
+  config.resolve.alias['eea-volto-theme-folder'] = hasDesignSystemInstalled
+    ? themeLessPath
+    : semanticLessPath;
 
   return config;
 };
